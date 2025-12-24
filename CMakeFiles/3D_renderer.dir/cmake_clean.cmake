c0ef18291f2339b43c55a87e68cc4e998916b0b1
Manu George <kuttan.mgc@gmail.com>
2025-12-24 16:56:05 +0800

    First commit

---
+++ CMakeFiles/3D_renderer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3D_renderer.dir/src/main.cpp.o"
+  "CMakeFiles/3D_renderer.dir/src/main.cpp.o.d"
+  "lib/lib3D_renderer.dylib"
+  "lib/lib3D_renderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3D_renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/3D_renderer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/manu/Projects/3D_renderer/lib/3D_renderer"
+  "/Users/manu/Projects/3D_renderer/lib/3D_renderer.pdb"
+  "CMakeFiles/3D_renderer.dir/src/main.cpp.o"
+  "CMakeFiles/3D_renderer.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3D_renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
